2dc1fcf17d5c0c640eb68fd780d8e17cb04c5949
Pavel Pavlenko <region23@gmail.com>
2023-05-30 09:19:08 +0300

    deploy config

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -2,8 +2,9 @@ import { defineConfig } from 'vitepress'
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
+  base: "/",
   title: "The Hitchhiker's Guide to the Ableton",
-  description: "Учебник по Ableton Live и созданию музыки в нём",
+  description: "Ещё один учебник по Ableton Live",
   themeConfig: {
     // https://vitepress.dev/reference/default-theme-config
     nav: [
